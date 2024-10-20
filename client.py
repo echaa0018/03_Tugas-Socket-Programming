d35b8c783f199d8c7cd766ab00248d6ed341634c
Ryuko <ryukomatoi@is.real>
2024-10-21 03:39:04 +0700

    Add password and quit features

--- client.py
+++ client.py
@@ -1,38 +1,24 @@
-# import socket
-# import threading
-# import random
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# client.bind(("localhost", random.randint(8000, 9000)))
-
-# name = input("Nickname: ")
-
-# def receive():
-#     while True:
-#         try:
-#             message, _ = client.recvfrom(1024)
-#             print(message.decode())
-#         except:
-#             pass
-
-# t = threading.Thread(target=receive)
-# t.start()
-
-# client.sendto(f"SIGNUP_TAG: {name}".encode(), ("localhost", 9999))
-
-# while True:
-#     message = input("")
-#     if message == "!q":
-#         exit()
-#     else:
-#         client.sendto(f"{name}: {message}".encode(), ("localhost", 9999))
-
-import socket 
+import socket
 import threading
 import random
+import sys
+import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(("localhost", random.randint(8000, 9000)))
+
+correct_password = "luvmakima"
+
+def authentication():
+    password = input("Password (1 attempt only): ")
+    if password != correct_password:
+        print("Incorrect Password, Goodbye")
+        sys.exit()
+    else:
+        print("Correct Password, please wait")
+        time.sleep(1)
+
+authentication()
 
 name = input("Nickname: ")
 
@@ -52,6 +38,12 @@ client.sendto(f"SIGNUP_TAG: {name}".encode(), ("localhost", 9999))
 while True:
     message = input("")
     if message == "!q":
-        break  # Changed from exit()
+        # Send quit message to the server
+        client.sendto(f"QUIT_TAG: {name}".encode(), ("localhost", 9999))
+        print("You have left the chatroom.")
+        break  # Exit the loop and terminate the client
     else:
+        # Send the message to the server
         client.sendto(f"{name}: {message}".encode(), ("localhost", 9999))
+        # Display the message on the client's own terminal
+        print(f"{name}: {message}")
